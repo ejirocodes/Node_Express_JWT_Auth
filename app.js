c063284fc193aa6b8b694c215ea9b4a56bc4420a
Ejiro Asiuwhu <Ejiroasiuwhu10@gmail.com>
2021-06-11 14:25:26 +0100

    add static files

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
+const PORT = 3000;
 const URL =
   'mongodb+srv://Ejiro:Ejiroghene10@cluster0.rsi5w.mongodb.net/Node_Express_Auth?retryWrites=true&w=majority';
 
@@ -12,11 +13,10 @@ app.set('view engine', 'ejs');
 mongoose
   .connect(URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    app.listen(3000, () => {
-      console.log('Server running');
+    app.listen(PORT, () => {
+      console.log(`DB connected! Server running on http://localhost:${PORT}`);
     });
   });
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', (req, res) => res.render('smoothies'));
